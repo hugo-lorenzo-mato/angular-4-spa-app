29c590f80fea7ba3218f61a3d856171218d34242
hugo-lorenzo-mato <hugo.lorenzomato@gmail.com>
2017-07-27 14:41:11 +0200

    Cambios menores de prueba

--- A4-SPA-APP/src/app/acerca-de/acerca-de.component.ts
+++ A4-SPA-APP/src/app/acerca-de/acerca-de.component.ts
@@ -2,38 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from "../data-service.service";
 
 @Component({
-  selector: 'app-acerca-de',
-  templateUrl: './acerca-de.component.html',
-  styleUrls: ['./acerca-de.component.css']
+    selector: 'app-acerca-de',
+    templateUrl: './acerca-de.component.html',
+    styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
 
-  public listado_ropa: Array<string>;
-  public prenda_a_guardar: string;
-  public prenda_a_eliminar: string;
+    public listado_ropa: Array<string>;
+    public prenda_a_guardar: string;
+    public prenda_a_eliminar: string;
 
-  constructor(
-    private _dataService: DataServiceService
-  ) { }
+    constructor(
+        private _dataService: DataServiceService
+    ) { }
 
-  ngOnInit() {
-    this.listado_ropa = this._dataService.getColeccion();
-    console.log(this._dataService.getPrenda());
-    console.log(this.listado_ropa);
+    ngOnInit() {
+        this.listado_ropa = this._dataService.getColeccion();
+        console.log(this._dataService.getPrenda());
+        console.log(this.listado_ropa);
 
-  }
-
-  guardarPrenda() {
-    if (this.prenda_a_guardar != null && this.prenda_a_guardar.toString) {
-      this._dataService.addRopa(this.prenda_a_guardar);
-      this.prenda_a_guardar = null;
     }
-  }
+
+    guardarPrenda() {
+        if (this.prenda_a_guardar != null && this.prenda_a_guardar.toString) {
+            this._dataService.addRopa(this.prenda_a_guardar);
+            this.prenda_a_guardar = null;
+        }
+    }
 
 
-  eliminarPrenda(index:number) {
-      this._dataService.eliminarRopa(index);
-  }
+    eliminarPrenda(index: number) {
+        this._dataService.eliminarRopa(index);
+    }
 
 
 }
